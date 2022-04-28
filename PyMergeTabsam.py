58e2a244d60705b6a300dcf6896b80d9c7cfaf79
Grüebler Michael (SSZ) <Michael.Grueebler@zuerich.ch>
2022-04-28 11:45:28 +0200

    Output wird anhand Vorlagedatei erstellt

--- PyMergeTabsam.py
+++ PyMergeTabsam.py
@@ -5,11 +5,16 @@
 import json
 import pandas as pd
 import datetime
+import shutil
 
 
 # Leere Listen vorbereiten
 data_files  = pd.DataFrame([],dtype=pd.StringDtype())
+data_sheets = pd.DataFrame([],dtype=pd.StringDtype())
 
+# Global variable from configuration
+path_input = ""
+filename_output = ""
 
 # Function tolog
 # Write logging information
@@ -25,7 +30,8 @@ def read_config():
   with open('config.json', 'r', encoding="utf-8") as f:
     config = json.load(f)
     list_files = []
-    global data_files, path_input, path_output
+    list_sheets = []
+    global data_files, data_sheets, path_input, filename_output
     
     path_input  = config['path_input']
     filename_output = config['filename_output']
@@ -33,7 +39,7 @@ def read_config():
     for key in config:
       conf_value = config[key]
       if key == "files":
-        # The collections are provided as a list. Read them an add them do the Dataframe data_coll
+        # The files are provided as a list. Read them an add them do the Dataframe data_files
         for i in range(len(conf_value)):
           files_elem = conf_value[i]
           pk = i+1
@@ -41,11 +47,43 @@ def read_config():
           elem_list_files = [pk, files_elem["title"], input_fullpath, files_elem["position"]]
           list_files.append(elem_list_files)
           data_files = pd.DataFrame(list_files, columns = ['id', 'title' , 'input_path', 'position'])
+      if key == "sheets":
+        # The sheets to process are provided as a list. Read them an add them do the Dataframe data_sheets
+        for i in range(len(conf_value)):
+          sheets_elem = conf_value[i]
+          pk = i+1
+          elem_list_sheets = [pk, sheets_elem["code"], sheets_elem["title"], sheets_elem["column"]]
+          list_sheets.append(elem_list_sheets)
+          data_sheets = pd.DataFrame(list_sheets, columns = ['id', 'code', 'title', 'column'])
+
+
+# Function create_tabsam 
+# Create and generate the destination excel files
+# If the destination files already exists, they will be overwritten
+def create_tabsam():
+  global filename_output
+
+  # Create empty output file based on template
+  tolog("INFO", "Writing output to: " + filename_output)
+  shutil.copy('VorlageTabsam.xlsx', filename_output)
+
+  sheet_id = 0
+  for sheet_i, sheet_row in data_sheets.iterrows():
+    sheet_id = sheet_row['id']
+    print(sheet_id)
+
+    for file_i, file_row in data_files.iterrows():
+      file_id = file_row['id']
+      print(file_id)
+    
 
 # Main progam
 def main():
   tolog("INFO", "Read the configuration")
   read_config()
+  
+  tolog("INFO", "Loop over the input sheets and files and merge the tables")
+  create_tabsam()
 
 
 # Execute main of PyTabsam
